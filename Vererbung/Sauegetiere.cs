eb039b1e19b8049ff70b507cda30bcc794a06e40
Chefsalah <salahadin.a@gso.schule.koeln>
2021-12-08 18:33:08 +0100

    first commit

---
+++ Vererbung/Sauegetiere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung
+{
+    internal class Sauegetiere : Tiere
+    {
+        protected double koerpertemperatur;
+
+        //Base Konstrucktor
+        protected Sauegetiere(int lebenserwartung, string bezeichnung, double koerpertemperatur) : base(lebenserwartung, bezeichnung)
+        {
+            this.koerpertemperatur = koerpertemperatur;
+        }
+        //Override abstrackt
+        protected override string Lebenserwartung()
+        {
+            return "" + base.lebenserwartung;
+        }
+        //Base
+        public string tierInfos()
+        {
+            return base.LebenserwartungUndBezeichnung();
+        }
+        //Override Virtual
+        protected override string LebenserwartungUndBezeichnung()
+        {
+            return base.LebenserwartungUndBezeichnung();
+        } 
+
+         
+    }
+}
---
+++ Vererbung/Tiere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung
+{
+     abstract class Tiere
+    {
+        protected int lebenserwartung;
+        protected string bezeichnung;
+       protected Tiere(int lebenserwartung,string bezeichnung)
+        {
+            this.lebenserwartung = lebenserwartung;
+            this.bezeichnung = bezeichnung;
+        }
+    
+        protected virtual  string LebenserwartungUndBezeichnung()
+        {
+            return lebenserwartung + bezeichnung;
+        }
+        protected abstract string Lebenserwartung();    
+    
+    }
+}
